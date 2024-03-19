84e765147b249b22d896b0024acb60a392bc3921
aboynejames <james@aboynejames.co.uk>
2024-03-19 14:08:12 +0000

    temp csv reader added parse for different dates

--- src/fileParser.js
+++ src/fileParser.js
@@ -19,6 +19,8 @@ import events from 'events'
 import axios from 'axios'
 import csv from 'csv-parser'
 import crypto from 'crypto'
+import { DateTime, Interval } from 'luxon'
+import * as chrono from 'chrono-node'
 
 var FileParser = function (path) {
   events.EventEmitter.call(this)
@@ -156,9 +158,14 @@ FileParser.prototype.TEMPwebCSVparse = function (fData) {
       // pairData.timestamp = splitRow[2]
       // pairData.price = splitRow[5]
       // console.log(pairData)
-      if (splitRow[2] !== undefined) {
+      if (splitRow[1] !== undefined) {
         extractCol.push(splitRow[fData.context.id])
-        extractLabel.push(splitRow[2])
+        // assume data column for now and parse to mills seconds
+        let testCH1 = chrono.parseDate(splitRow[fData.context.timestamp])
+        let parseDate = this.testDataExtact(testCH1)
+        // let parseDate = DateTime.fromISO(splitRow[0])
+        // let millDate = parseDate.toMillis()
+        extractLabel.push(parseDate)
       }
     }
   })
@@ -166,8 +173,28 @@ FileParser.prototype.TEMPwebCSVparse = function (fData) {
   let extractedPair = {}
   extractedPair.label = extractLabel
   extractedPair.data = extractCol
+  console.log('finsiehd')
+  console.log(extractedPair)
   return extractedPair
+}
 
+/**
+* try to match to known time formats
+* @method testDataExtact
+*
+*/
+FileParser.prototype.testDataExtact = function (sampleDate) {
+  let parseDate0 = DateTime.fromISO(sampleDate)
+  let parseDate1 = DateTime.fromHTTP(sampleDate)
+  let parseDate2 = DateTime.fromJSDate(sampleDate)
+  /* let parseDate3 = DateTime.fromFormat(sampleDate, "dd-MM-yyyy HH:MM")
+  console.log(parseDate3)
+  let parseDate4 = DateTime.local(sampleDate)
+  console.log(parseDate4)
+  // .fromJSDate(sampleDate) // .fromHTTP(sampleDate) //  fromFormat(sampleDate, "YYY-MM-DD ")  //.fromISO(sampleDate) // or DateTime. fromFormat("23-06-2019", "dd-MM-yyyy") .(splitRow[0])//  new Date(splitRow[0])
+  // console.log(parseDate) */
+  let millDate = parseDate2.toMillis()
+  return millDate
 }
 
 /**
